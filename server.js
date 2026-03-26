97af9a7b33ba1dca28c66b4dd1ef65f74b080e68
Prathamesh2228 <jayapitu2209@gmail.com>
2026-03-26 13:34:23 +0530

    fix cors for production

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://unica-frontend-eight.vercel.app', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
